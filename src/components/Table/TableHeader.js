65d9971019f35c5d6985c6026d2bd88c560198ba
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-09-09 10:33:10 +0800

    Fix right margin of App not showing.

--- src/components/Table/TableHeader.js
+++ src/components/Table/TableHeader.js
@@ -3,26 +3,26 @@ import "./TableHeader.css";
 export default function TableHeader() {
 	return (
 		<tr className="TableHeader">
-			<th className="columnHeader">D.R. Number</th>
-			<th className="columnHeader">P.O. Number</th>
-			<th className="columnHeader">Status</th>
-			<th className="columnHeader">Supplier</th>
-			<th className="columnHeader">Industry</th>
-			<th className="columnHeader">Category</th>
-			<th className="columnHeader">SKU's</th>
-			<th className="columnHeader">Quantity Delivered</th>
-			<th className="columnHeader">Quantity Accepted</th>
-			<th className="columnHeader">Amount Delivered</th>
-			<th className="columnHeader">Amount Accepted</th>
-			<th className="columnHeader">Type</th>
-			<th className="columnHeader">Delivery Date</th>
-			<th className="columnHeader">ETA</th>
-			<th className="columnHeader">Date Created</th>
-			<th className="columnHeader">Point Person Surname</th>
-			<th className="columnHeader">Point Person First Name</th>
-			<th className="columnHeader">Point Person Username</th>
-			<th className="columnHeader">Point Person ID</th>
-			<th className="columnHeader">Color</th>
+			<th>D.R. Number</th>
+			<th>P.O. Number</th>
+			<th>Status</th>
+			<th>Supplier</th>
+			<th>Industry</th>
+			<th>Category</th>
+			<th>SKU's</th>
+			<th>Quantity Delivered</th>
+			<th>Quantity Accepted</th>
+			<th>Amount Delivered</th>
+			<th>Amount Accepted</th>
+			<th>Type</th>
+			<th>Delivery Date</th>
+			<th>ETA</th>
+			<th>Date Created</th>
+			<th>Point Person Surname</th>
+			<th>Point Person First Name</th>
+			<th>Point Person Username</th>
+			<th>Point Person ID</th>
+			<th>Color</th>
 		</tr>
 	)
 }
--- src/components/Table/TableRow.js
+++ src/components/Table/TableRow.js
@@ -4,33 +4,34 @@ export default function TableRow(props) {
 	let data = props.data;
 	return (
 		<tr className="TableRow">
-			<td className="rowData">{data.dr}</td>
-			<td className="rowData">{data.po}</td>
-			<td className="rowData outlined">{makeReadable(data.status, "Delivered", "In Transit")}</td>
-			<td className="rowData">{data.supplier}</td>
-			<td className="rowData">{data.industry}</td>
-			<td className="rowData">{data.category}</td>
-			<td className="rowData">{data.sku_amt}</td>
-			<td className="rowData">{data.qty_delivered}</td>
-			<td className="rowData">{data.qty_accepted}</td>
-			<td className="rowData">₱{data.amt_delivered}</td>
-			<td className="rowData">₱{data.amt_accepted}</td>
-			<td className="rowData outlined">{makeReadable(data.type, "Full", "Partial")}</td>
-			<td className="rowData">{data.delivery_date}</td>
-			<td className="rowData">{data.eta}</td>
-			<td className="rowData">{data.creation_date}</td>
-			<td className="rowData">{data.point_person_surname}</td>
-			<td className="rowData">{data.point_person_firstname}</td>
-			<td className="rowData">{data.point_person_username}</td>
-			<td className="rowData">{data.point_person_id}</td>
-			<td className="rowData" style={{backgroundColor: data.color}}>{data.color}</td>
+			<td>{data.dr}</td>
+			<td>{data.po}</td>
+			<td className="outlined centered">
+				<div>
+					{(data.status) ? "Delivered" : "In Transit"}
+				</div>
+			</td>
+			<td>{data.supplier}</td>
+			<td>{data.industry}</td>
+			<td>{data.category}</td>
+			<td>{data.sku_amt}</td>
+			<td>{data.qty_delivered}</td>
+			<td>{data.qty_accepted}</td>
+			<td>₱{data.amt_delivered}</td>
+			<td>₱{data.amt_accepted}</td>
+			<td className="outlined centered">
+				<div>
+					{(data.type) ? "Full" : "Partial"}
+				</div>
+			</td>
+			<td>{data.delivery_date}</td>
+			<td>{data.eta}</td>
+			<td>{data.creation_date}</td>
+			<td>{data.point_person_surname}</td>
+			<td>{data.point_person_firstname}</td>
+			<td>{data.point_person_username}</td>
+			<td>{data.point_person_id}</td>
+			<td style={{backgroundColor: data.color}}>{data.color}</td>
 		</tr>
 	);
-}
-
-function makeReadable(bool, onTrue, onFalse) {
-	if (bool) {
-		return onTrue;
-	}
-	return onFalse;
 }
