2a2e0ba85ae029c4a1baef4603659858b69cddce
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-09-10 18:33:16 +0800

    Add 5 more rows of data. Add pagination functionality.

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -7,39 +7,48 @@ import { useState } from "react";
 const ITEMS_PER_PAGE = 10;
 
 export default function Table(props) {
-	let [page, setPage] = useState(0);
+	let [currentPage, setPage] = useState(0);
 
-	let numberOfPages = Math.floor(props.data.length / ITEMS_PER_PAGE);
-	let slicedData = props.data.slice(
-		(page) * ITEMS_PER_PAGE,
-		(page + 1) * ITEMS_PER_PAGE
-	);
+	let numberOfPages = Math.ceil(props.data.length / ITEMS_PER_PAGE);
+	
+	let slicedData = [];
+	for (let page = 0; page < numberOfPages; page++) {
+		slicedData.push(props.data.slice(
+			(page) * ITEMS_PER_PAGE,
+			(page + 1) * ITEMS_PER_PAGE
+		));
+	}
 
+	function movePage(pageNumber) {
+		if (pageNumber < 0 || pageNumber > numberOfPages - 1) return;
+		setPage(pageNumber);
+	}
+
+	function createPageButtons(numberOfPages) {
+		let pageButtons = [];
+		for (let i = 0; i < numberOfPages; i++) {
+			pageButtons.push(
+				<button className="pageButton" onClick={() => movePage(i)}>{i + 1}</button>
+			);
+		}
+		return pageButtons;
+	}
+		
 	return (
 		<div className="Table">
 			<table>
 					<TableHeader />
 					<tbody>
-						{slicedData.map((item) => {
+						{slicedData[currentPage].map((item) => {
 							return (<TableRow data={item}/>);
 						})}
 					</tbody>
 			</table>
 			<div className="pagination">
-				<button className="previousPage">&lt;</button>
+				<button className="previousPage" onClick={() => movePage(currentPage - 1)}>&lt;</button>
 				{createPageButtons(numberOfPages)}
-				<button className="nextPage">&gt;</button>
+				<button className="nextPage" onClick={() => movePage(currentPage + 1)}>&gt;</button>
 			</div>
 		</div>
 	);
-}
-
-function createPageButtons(numberOfPages) {
-	let pageButtons = [];
-	for (let i = 0; i < numberOfPages; i++) {
-		pageButtons.push(
-			<button className="pageNumber">{i + 1}</button>
-		);
-	}
-	return pageButtons;
 }
